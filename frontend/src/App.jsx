54b59fa80f18de1f787604620dc2878d36155327
Gan Wen Tao <umawanbong@gmail.com>
2024-07-06 12:56:30 +0800

    Setup react bootstrap and tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import { Button } from 'react-bootstrap';
 
 function App() {
 
   return (
     <>
-      <h1>HorFunNy</h1>
+      <h1 className='text-3xl font-bold underline text-clifford'>HorFunNy</h1>
+      <Button>Test</Button>
     </>
   )
 }
