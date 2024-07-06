a89d3801c15b41361c09b28ebbd4bd7c713a5a05
Gan Wen Tao <umawanbong@gmail.com>
2024-07-06 22:25:51 +0800

    Create UI button and input

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import UIButton from "../components/UIButton";
+import UIInput from "../components/UIInput";
 
 function Home() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-3xl font-bold underline text-clifford">HorFunNy</h1>
-      <Button>Test</Button>
+      <UIButton variant="outline-primary" size="lg" className="my-5">
+        Test
+      </UIButton>
+      <div className="w-1/3">
+        <UIInput
+          id="username"
+          label="Username"
+          type="text"
+          placeholder="Username"
+        />
+      </div>
     </div>
   );
 }
