b2902f405e826d511a9a8d92feaa37cd60b66273
Gan Wen Tao <umawanbong@gmail.com>
2024-07-06 17:46:43 +0800

    Setup pre-commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
-import { Button } from 'react-bootstrap';
+import React from "react";
+import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-3xl font-bold underline text-clifford'>HorFunNy</h1>
+      <h1 className="text-3xl font-bold underline text-clifford">HorFunNy</h1>
       <Button>Test</Button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
