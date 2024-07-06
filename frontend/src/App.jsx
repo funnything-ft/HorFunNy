9ecc2666a5c41d161ad8260cf8e515b965cbc139
Gan Wen Tao <umawanbong@gmail.com>
2024-07-06 19:51:32 +0800

    Develop navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.css";
 import { Button } from "react-bootstrap";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-clifford">HorFunNy</h1>
-      <Button>Test</Button>
+      <Header />
+      <main className="flex justify-center">
+        <div className="flex flex-col items-center">
+          <h1 className="text-3xl font-bold underline text-clifford">
+            HorFunNy
+          </h1>
+          <Button>Test</Button>
+        </div>
+      </main>
     </>
   );
 }
