243f70ddb158205d02998c69a9c8dcb866fe1fed
wentaogan <wentao.gan@1cloudstar.com>
2024-07-10 15:59:52 +0800

    Create error page

--- frontend/src/router/routes.jsx
+++ frontend/src/router/routes.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/Layout";
 import Home from "../pages/Home";
+import Error from "../pages/Error";
 import Login, { action as loginAction } from "../pages/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <Error />,
     children: [
       {
         index: true,
