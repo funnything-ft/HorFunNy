cb4424094aa617f084847d5b21a51b44b0964382
Gan Wen Tao <umawanbong@gmail.com>
2024-07-15 00:30:23 +0800

    Integrate frontend & backend for update profile detail

--- frontend/src/components/ProfileForm.jsx
+++ frontend/src/components/ProfileForm.jsx
@@ -1,45 +1,81 @@
-import React from "react";
-import { useRouteLoaderData } from "react-router-dom";
+import React, { useState } from "react";
+import { Form, useRouteLoaderData, json } from "react-router-dom";
 import UIInput from "../components/UIInput";
 import UIButton from "../components/UIButton";
 import UITextareaInput from "../components/UITextareaInput";
 import UISelectInput from "../components/UISelectInput";
+import apiInstance from "../utils/axios";
 
-function ProfileForm() {
+const genderOptions = {
+  Male: "M",
+  Female: "F",
+  Unknown: "U",
+};
+
+function ProfileForm({ action }) {
   const { name, desc, gender } = useRouteLoaderData("profile-detail");
-  const genderOptions = {
-    Male: "M",
-    Female: "F",
-    Unknown: "U",
-  };
+  const [userInput, setUserInput] = useState({
+    name,
+    desc,
+    gender,
+  });
+  function handleInputChange(identifier, e) {
+    setUserInput((prev) => ({
+      ...prev,
+      [identifier]: e.target.value,
+    }));
+  }
   return (
     <div className="px-14">
-      <p className="font-bold">Basic Info: </p>
-      <UIInput
-        label="Name"
-        id="name"
-        type="text"
-        value={name}
-        className="mb-4"
-      />
-      <UITextareaInput
-        label="Description"
-        id="desc"
-        value={desc}
-        className="mb-4"
-      />
-      <UISelectInput
-        options={genderOptions}
-        label="Gender"
-        id="gender"
-        value={gender}
-        className="mb-4"
-      />
-      <UIButton className="w-32 mb-4" variant="primary" size="lg" type="button">
-        Save
-      </UIButton>
+      <Form method="put">
+        <p className="font-bold">Basic Info: </p>
+        <UIInput
+          label="Name"
+          id="name"
+          type="text"
+          value={userInput.name}
+          className="mb-4"
+          onChange={(e) => handleInputChange("name", e)}
+        />
+        <UITextareaInput
+          label="Description"
+          id="desc"
+          value={userInput.desc}
+          className="mb-4"
+          onChange={(e) => handleInputChange("desc", e)}
+        />
+        <UISelectInput
+          options={genderOptions}
+          label="Gender"
+          id="gender"
+          value={userInput.gender}
+          className="mb-4"
+          onSelectChange={(e) => handleInputChange("gender", e)}
+        />
+        <UIButton
+          className="w-32 mb-4"
+          variant="primary"
+          size="lg"
+          type="submit"
+          name="action"
+          value={action}
+        >
+          Save
+        </UIButton>
+      </Form>
     </div>
   );
 }
 
 export default ProfileForm;
+
+export async function UpdateProfile(data) {
+  return apiInstance
+    .put("profile/edit/detail/", data)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((err) => {
+      throw json({ message: err.response.data.message }, { status: 500 });
+    });
+}
--- frontend/src/components/UISelectInput.jsx
+++ frontend/src/components/UISelectInput.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 
-function UISelectInput({ options, label, id, className, value }) {
+function UISelectInput({
+  options,
+  label,
+  id,
+  className,
+  value,
+  onSelectChange,
+}) {
   return (
     <div className={className}>
       <Form.Label htmlFor={id} className="font-bold">
         {label}
       </Form.Label>
-      <Form.Select aria-label={id} id={id} value={value}>
+      <Form.Select
+        aria-label={id}
+        id={id}
+        name={id}
+        value={value}
+        onChange={onSelectChange}
+      >
         {Object.entries(options).map(([key, value]) => (
           <option key={value} value={value}>
             {key}
