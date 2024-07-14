4048443537781e5b78c56f1f5db4dd0985bfea89
Gan Wen Tao <umawanbong@gmail.com>
2024-07-14 18:43:39 +0800

    Develop upload image form & integrate frontend

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -1,24 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Table, Container, Image } from "react-bootstrap";
-import { useRouteLoaderData } from "react-router-dom";
+import { useNavigate, useParams, useRouteLoaderData } from "react-router-dom";
 import ProfileForm from "../components/ProfileForm";
+import ProfileUploadPhotoForm from "../components/ProfileUploadPhotoForm";
 
 export default function EditProfile() {
   const { image, user } = useRouteLoaderData("profile-detail");
+  const { form } = useParams();
+  const navigate = useNavigate();
+  const forms = {
+    profile: <ProfileForm />,
+    photo: <ProfileUploadPhotoForm />,
+  };
   const sectionListClass = "hover:bg-slate-200 text-center font-semibold py-2";
   return (
     <Container>
       <Table bordered size="sm">
         <tbody>
           <tr>
-            <td rowSpan={2}>
+            <td rowSpan={2} className="w-56">
               <div className="w-14 mx-auto mt-4">
                 <Image src={image} roundedCircle className="object-cover" />
               </div>
               <p className="text-center font-bold">{user.username}</p>
               <ul className="p-0 grid grid-cols gap-2">
-                <li className={sectionListClass}>Profile</li>
-                <li className={sectionListClass}>Photo</li>
+                <li
+                  className={`${sectionListClass} ${
+                    form === "profile" ? "bg-slate-200" : ""
+                  }`}
+                  onClick={() => navigate("/profile/edit/profile")}
+                >
+                  Profile
+                </li>
+                <li
+                  className={`${sectionListClass} ${
+                    form === "photo" ? "bg-slate-200" : ""
+                  }`}
+                  onClick={() => navigate("/profile/edit/photo")}
+                >
+                  Photo
+                </li>
               </ul>
             </td>
             <td className="text-center">
@@ -27,9 +48,7 @@ export default function EditProfile() {
             </td>
           </tr>
           <tr>
-            <td>
-              <ProfileForm />
-            </td>
+            <td>{forms[form]}</td>
           </tr>
         </tbody>
       </Table>
