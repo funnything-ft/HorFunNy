842d1b18cdf893fc0eaa729aafbe32d126d13a8c
wentaogan <wentao.gan@1cloudstar.com>
2024-07-18 15:52:34 +0800

    Implement protected route using redux

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -1,11 +1,13 @@
+import { dispatch } from "../store";
+import { authActions } from "../store/auth-slice";
 import apiInstance from "../utils/axios";
-import { json, redirect } from "react-router-dom";
+import { json } from "react-router-dom";
 
 export function action() {
   return apiInstance
     .post("logout/")
     .then(() => {
-      return redirect("/");
+      return dispatch(authActions.logout());
     })
     .catch((err) => {
       throw json({ message: err.response.data.message }, { status: 500 });
