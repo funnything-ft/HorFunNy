d489e29ade57a655489150a9bdd9634f0ff67d41
Gan Wen Tao <umawanbong@gmail.com>
2024-07-19 18:27:37 +0800

    Create post model

--- backend/multimedia/models.py
+++ backend/multimedia/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from userauth.models import User
+
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    caption = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    image = models.ImageField(upload_to='posts/')
+
+    def __str__(self):
+        return f'{self.caption[:50]}'
