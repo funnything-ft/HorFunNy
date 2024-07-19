9f37b67e2deb7005a594ed51014c34bad3c2e0c5
Gan Wen Tao <umawanbong@gmail.com>
2024-07-19 21:49:05 +0800

    Develop view for create post

--- backend/multimedia/views.py
+++ backend/multimedia/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from . import serializers
+
+
+class CreatePostView(generics.CreateAPIView):
+    permission_classes = (IsAuthenticated, )
+    serializer_class = serializers.CreatePostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
