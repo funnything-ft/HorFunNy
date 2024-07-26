ed4a5c8057f709d9a6686fb5217d0517fd091ebe
Gan Wen Tao <umawanbong@gmail.com>
2024-07-26 15:51:22 +0800

    Develop retrieve posts in profile page for backend

--- backend/multimedia/views.py
+++ backend/multimedia/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 
 from . import serializers
+from .models import Post
 
 
 class CreatePostView(generics.CreateAPIView):
@@ -11,3 +12,12 @@ class CreatePostView(generics.CreateAPIView):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+class RetrievePostView(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, )
+    serializer_class = serializers.PostSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Post.objects.filter(author=user)
